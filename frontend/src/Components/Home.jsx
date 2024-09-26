622be2113809e83a23a15a6564b54397ef11707d
james victor <victorjames40v@gmail.com>
2024-09-26 23:30:30 +0100

    create landing page: create the header and the body

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -2,8 +2,15 @@
 
 const Home = () => {
     return (
-      <div className="text-3xl font-extrabold items-center">Our Ui</div>
-    )
+      <div className="text-3xl font-extrabold items-center  w-[100%]  flex justify-center mt-[13%]">
+        <div className="w-[80%] flex flex-col justify-center align-middle items-center">
+          <p className="text-xl leading-6 font-[] text-white">Empowering Financial Freedom, One Transaction at a Time</p>
+          <h2 className="text-6xl text-center leading-[68px] font-bold text-white">
+            Borrow or Lend <span className="text-[#3E8BFF]">Tokens</span> with ease and transparency
+          </h2>
+        </div>
+      </div>
+    );
   }
   
   export default Home
