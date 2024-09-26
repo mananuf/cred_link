9760f80bcdcdfa11b8bad8dd35c9a0c5ca343e0f
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-09-26 21:45:48 +0100

    add state variables

--- contract/contracts/CredLink.sol
+++ contract/contracts/CredLink.sol
@@ -5,6 +5,39 @@ pragma solidity ^0.8.27;
 // import "hardhat/console.sol";
 
 contract CredLinkContract {
+    address owner;
+    mapping (address => uint) availableLoans;
+    mapping (address => borrowerDetails[]) interestedBorrowers;
+    mapping (address => borrowerDetails[]) approvedBorrowers;
     
+    struct borrowerDetails {
+        address borrowerAddress;
+        uint loanId;
+        uint duration;
+        uint amount;
+    }
+
+    // setter functions
+
+    function lenderDeposit(uint _amount) external {}
+
+    function approveBorrower(address _borrowerToApprove, uint _amount) external {}
+
+    function applyForLoan(uint _loanId, uint _duration, uint _amount) external {}
+
+    function repayLoan(uint loanId) external {}
+
+
+    // getter functions
+    function viewInterestedBorrowers() external {}
+
+    function viewAvailableLoans() external {}
+
+    function viewApproveBorrowers() external {}
+
+    function openDispute() external {}
+
     
+
+
 }
--- contract/contracts/lib/Events.sol
+++ contract/contracts/lib/Events.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.27;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-libary Events {
+library Events {
     
 }
