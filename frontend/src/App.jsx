f71b9e8c651491804de51df6e4eccfbc8b451d8c
King Jaja <jajasamson@yahoo.com>
2024-09-26 17:16:46 -0700

    implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,15 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home";
+import LenderDashboard from "./Components/LenderDashboard";
 import Root from "./Root";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root/>}>
-        <Route index element={<Home/>}/>
+        <Route index element={<LenderDashboard/>}/>
+        {/* <Route path="LenderDAshboard" element={<LenderDashboard/>}/> */}
       </Route>
 
     )
---
+++ frontend/src/Components/Borrow.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Borrow = () => {
+  return (
+    <div>
+      <h2>Set Minimum Balance Requirement</h2>
+      <p>Total fiat balance</p>
+      <h1>$20,000.00</h1>
+      <div>
+        <p>Min Amount:</p>
+        <input type="text" />
+      </div>
+      <hr />
+      <button>Borrow <img src={} alt="" /></button>
+    </div>
+  )
+}
+
+export default Borrow
